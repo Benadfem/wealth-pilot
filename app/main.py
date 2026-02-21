f009bb38d0bda2300301d9300e0367469e504648
HomePC <adedoyin.adedara@gmail.com>
2026-02-21 14:09:05 +0100

    Lesson 1: First FastAPI endpoint working

---
+++ app/main.py
@@ -0,0 +1,16 @@
+# We're going to build a simple API with ONE endpoint that responds with a message. '
+# 'That's it. Nothing fancy. Just the absolute basics. 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# to get request from the server, use the decorator as below 
+@app.get("/")
+# declare the function to perform the get request 
+def read_root():
+    return {"message": "Hello from Wealth-pilot"}
+
+@app.get("/health")
+def read_root():
+    return {"Health": "this is healthy"}
