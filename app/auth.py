b790c37e64007080def8dfc26450d516adae5250
HomePC <adedoyin.adedara@gmail.com>
2026-03-12 01:23:43 +0100

    Lesson 7: Completed auth.py with hash, verify and token functions

--- app/auth.py
+++ app/auth.py
@@ -7,3 +7,19 @@ ALGORITHM ="HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# define the hash_password
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+# define the verify password
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# creating the access token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
