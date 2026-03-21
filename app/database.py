aeaf743befb4fb49322e5ea8cb0ebe18d50d7993
HomePC <adedoyin.adedara@gmail.com>
2026-03-21 13:20:27 +0100

    Lesson 10: Updated database.py to use environment variables

--- app/database.py
+++ app/database.py
@@ -2,7 +2,12 @@ from sqlalchemy  import create_engine # type: ignore
 from sqlalchemy.orm import declarative_base # type: ignore
 from sqlalchemy.orm  import sessionmaker # type: ignore
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/wealth_pilot" 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/wealth_pilot")
 
 engine = create_engine(DATABASE_URL)
 
